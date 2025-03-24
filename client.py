2eeae62c69bf45dd25e1a9ed1813db39e22d952f
Gabriel Venancio <88842379+wazeveve@users.noreply.github.com>
2025-03-24 08:31:58 -0300

    Commit 2
    Configuração inicial do client.py

---
+++ client.py
@@ -0,0 +1,14 @@
+import socket as s
+
+HOST_SERVER = '127.0.0.1'
+PORT_SERVER = '50000'
+
+with s.socket(s.AF_INET, s.SOCK_STREAM) as client:
+
+    client.connect((HOST_SERVER, PORT_SERVER))
+
+    client.send("Bom dia Servidor!")
+
+    dataServer = client.recv(1024).decode('utf-8')
+
+print(f"Dado recebido do servidor: {dataServer}")
